72596671df38d296fb128bfd08e53374f82ced2f
liuzel01 <liuzel01@outlook.com>
2021-02-26 17:07:28 +0800

    mod, 小格式

--- dockerdev/c7ssh_10022/start_build.sh
+++ dockerdev/c7ssh_10022/start_build.sh
@@ -5,7 +5,7 @@
 cat ~/.ssh/id_rsa.pub  > ~/.ssh/authorized_keys
 [ $? -eq 0 ] && \cp ~/.ssh/authorized_keys ./authorized_keys && \
 docker build -t c7sshd_10022 . -f Dockerfile && \
-docker run -itd -p 10022:22 --name lzl_c7ssh c7sshd_10022
+docker run -itd -p 10022:22 --name lzl_c7ssh c7sshd_10022:lzl
 
 [ $? -eq 0 ] && docker ps --no-trunc | grep lzl_c7ssh
 # docker cp authorized_keys  lzl_c7ssh:/root/.ssh/authorized_keys
