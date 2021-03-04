4ba443f5abb5d2b62f0d25eb234663e172903412
liuzel01 <liuzelin01@outlook.com>
2021-03-04 10:05:53 +0800

    add, 使用外网链接，& 开启端口

--- shellInstall/smartone/mariadbInstall.sh
+++ shellInstall/smartone/mariadbInstall.sh
@@ -3,43 +3,49 @@
 # TODOList:
 # 1. 将手动输入的maria_ver, 与官网比较，测验是否存在
 # 2. 测试，服务器上是否有安装mariadb
-# 3. 开启端口
 # 3. 检测，安装前，需要的环境准备，或者说优化部分
 # 4. 判断是否有 mariadb 运行， 并备份后 卸载
     # yum list installed | grep MariaDB
     # yum remove MariaDB -y
     # rm -rf /var/lib/mysql/*
-# 5. 设置开机自启
 
 # 判断，mariadb.repo 是否存在
-mari=`find . ! -name "." -type d -prune -o -type f -name "*.repo" -print | grep -i mariadb`
-mari_ver=10.1
+MARI=`find . ! -name "." -type d -prune -o -type f -name "*.repo" -print | grep -i mariadb`
+MARI_VER=10.1
 cd /etc/yum.repos.d
-if [ ! -f "$mari" ];then
+if [ ! -f "$MARI" ];then
 	echo 'there is no mariadb.repo'
 	touch MariaDB.repo && echo 'OK,there is~'
 else
-	\cp $mari MariaDB.repo.bak 2>/dev/null
-	# echo $(find . ! -name "." -type d -prune -o -type f  -print | grep -i mariadb)
+	mv $MARI MariaDB.repo.bak 2>/dev/null
 fi
-
 # 修改repo，直接覆盖，因为上面已备份原文件了
 cat > /etc/yum.repos.d/MariaDB.repo << EOF
 [mariadb]
 name = MariaDB
-baseurl = http://mirrors.ustc.edu.cn/mariadb/yum/$mari_ver/centos7-amd64/
+baseurl = http://mirrors.ustc.edu.cn/mariadb/yum/$MARI_VER/centos7-amd64/
 gpgkey = http://mirrors.ustc.edu.cn/mariadb/yum/RPM-GPG-KEY-MariaDB
 gpgcheck = 1
 EOF
-
-# 开始安装，repo中已指定版本，可修改 mari_ver
-# 并，加入开机自启动
+# 开始安装，repo中已指定版本，可修改 MARI_VER
 yum clean all
 yum install MariaDB-server MariaDB-client -y
 sleep 3
-systemctl start mariadb &&\
-    systemctl enable mariadb
+systemctl enable mariadb &&\
+    echo '已设置开机自启' &&\
+    echo '检测是否安装成功' &&\
+    systemctl start mariadb
 
 # 测试是否安装，并启动成功
-[ $? -eq 0 ] && echo 'mariadb 安装成功'|| echo 'mariadb 安装失败' &&\
-        systemctl status mariadb 2>/dev/null
+[ $? -eq 0 ] && echo 'mariadb 安装成功' || echo 'mariadb 安装失败'
+# 开启端口， 3306
+open_port() {
+firewall-cmd --list-ports &>/dev/null
+[ $? -ne 0 ] && echo '请检查防火墙是否运行，再执行以下： ' &&\
+        echo 'firewall-cmd --zone=public --add-port=3306/tcp --permanent' &&\
+        echo 'firewall-cmd --reload' || firewall-cmd --zone=public --add-port=3306/tcp --permanent &>/dev/null &&\
+        firewall-cmd --reload &>/dev/null &&\
+        echo '检查端口是否添加成功： ' `firewall-cmd --zone=public --query-port=3306/tcp`
+}
+
+open_port
--- shellInstall/smartone/minio.sh
+++ shellInstall/smartone/minio.sh
@@ -8,7 +8,9 @@ MINIO_HOST=127.0.0.1
 MINIO_PORT=9000
 
 mkdir -p $MINIO_DIR/data
-cp ./packages/* $MINIO_DIR/
+wget http://meeting.sipingsoft.com/smart/mc -P $MINIO_DIR/
+wget http://meeting.sipingsoft.com/smart/minio -P $MINIO_DIR/
+chmod +x $MINIO_DIR/mc $MINIO_DIR/minio
 touch $MINIO_DIR/minio.log
 
 # 添加进环境变量
@@ -21,5 +23,35 @@ eof
 # 后台指定参数运行
 cd $MINIO_DIR
 source /etc/profile
+# 开启端口， 9000
+open_port() {
+firewall-cmd --list-ports &>/dev/null
+[ $? -ne 0 ] && echo '请检查防火墙是否运行，再执行以下： ' &&\
+        echo 'firewall-cmd --zone=public --add-port=9000/tcp --permanent' &&\
+        echo 'firewall-cmd --reload' || firewall-cmd --zone=public --add-port=9000/tcp --permanent &>/dev/null &&\
+        firewall-cmd --reload &>/dev/null &&\
+        echo '检查端口是否添加成功： ' `firewall-cmd --zone=public --query-port=9000/tcp`
+}
+
+open_port
+# 设置成开机自启
+touch /etc/init.d/minio
+chmod +x /etc/init.d/minio
+cat > /etc/init.d/minio <<salute
+#!/bin/sh
+#
+# minio - this script starts and stops the minio daemon
+#
+# chkconfig:   - 85 15
+# description:  minio is
+# processname: minio
+
+cd $MINIO_DIR ;nohup ./minio server $MINIO_DIR/data/ &
+salute
+chkconfig --add minio &&\
+    chkconfig minio on
+chkconfig --list | grep minio &>/dev/null &&\
+    [ $? -eq 0 ] &&\
+    echo 'minio已设置为开机自启'
 
 nohup ./minio server --address "${MINIO_HOST}:${MINIO_PORT}" $MINIO_DIR/data  >> $MINIO_DIR/minio.log 2>&1 &
