8cb62a8dd5ba3ce9943dc8165bd656fb249cd486
liuzel01 <liuzelin01@outlook.com>
2021-03-11 19:55:21 +0800

    feat: 新增"curl 上传备份到ftp服务器"

---
+++ shellInstall/backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# curl -X/--request 指定什么命令
+	# -T/--upload-file 上传文件
+
+TIME=`date +%Y-%m-%d_%H-%M-%S`
+MONTIME=`date +%Y-%m`
+SQLFILE='/tmp/192.168.10.107-db-'$TIME'.sql.gz'
+FILES='/tmp/192.168.10.107-files-'$TIME'.tar.gz'
+
+tar zcvf $FILES /home/fdfs_storage/data/
+mysqldump -usiping -pbackup.com sksdb_crm | gzip -c > $SQLFILE
+
+curl -u "bak_upload:abc123." ftp://192.168.10.25/ -X "MKD "$MONTIME
+curl -u "bak_upload:abc123." ftp://192.168.10.25/$MONTIME/ -T "{$FILES,$SQLFILE}"
+
+rm -rf  $SQLFILE $FILES
+
+echo "Backup completed."
