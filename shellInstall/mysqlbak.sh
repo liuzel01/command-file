9c2ae705d2561104f6365d436bac372e92d46ae6
liuzel01 <liuzelin01@outlook.com>
2021-03-12 11:20:14 +0800

    perf: 变量大写，新增del_bef_10d, curl上传备份文件到win-ftp

--- shellInstall/mysqlbak.sh
+++ shellInstall/mysqlbak.sh
@@ -1,20 +1,58 @@
 #2018.06.09
 #定义用户名和密码
-username="root"
-password="123456"
+MYSQL_USER="root"
+MYSQL_PASS="sksHykf8gw6l8bfs3ef"
 #设置备份目录，在此为/mysqlbak，可自行设置
-backup_dir="/mysqlbak"
+BACKUP_DIR="/home/mysqlbak"
 #获取系统时间格式201806091420
-backuptime="$(date +"%Y%m%d%H")"
+TIME="$(date +"%Y%m%d%H%M")"
 #删除时间设置为当前时间前2周
-deletetime=`date -d "2 week ago" +"%Y%m%d%H"`
-rm -f $backup_dir/mysqlbak_$deletetime.zip
-#进入mysql可执行文件目录，本人mysql安装在/usr/local/mysql
-cd /usr/local/mysql/bin
-#执行导出全库语句
-# ./mysqldump -u$username -p$password --all-databases> "$backup_dir"/mysql_"$backuptime.sql"
-# 一般的，如果数据库过多， 就需要单独备份，这样还原也好做
-./mysqldump -u$username -p$password  smartone_nacos --skip-lock-tables         > "$backup_dir"/smartone_nacos_"$backuptime.sql"
+DELETETIME=`date -d "2 week ago" +"%Y%m%d%H"`
+# config for ftp
+FTP_IP="192.168.4.247"
+FTP_USER="Administrator"
+FTP_PASS="Admin@bzzn"
+FTP_DIR="mysqlbak"
 
-zip -r $backup_dir/mysqlbak_$backuptime.zip  $backup_dir/*.sql
-[ $? -eq 0 ] && rm -rf $backup_dir/*.sql
+# 这个可能不太对
+# rm -f $BACKUP_DIR/mysqlbak_$DELETETIME.zip
+#进入mysql可执行文件目录，本人mysql安装在/usr/local/mysql
+bak_mysql() {
+cd /usr/bin
+# ./mysqldump -u$MYSQL_USER -p$MYSQL_PASS --all-databases> "$BACKUP_DIR"/mysql_"$TIME.sql"
+./mysqldump -u$MYSQL_USER -p$MYSQL_PASS  smartone_common --skip-lock-tables         > "$BACKUP_DIR"/smartone_common_"$TIME.sql"
+./mysqldump -u$MYSQL_USER -p$MYSQL_PASS  smartone_nacos --skip-lock-tables         > "$BACKUP_DIR"/smartone_nacos_"$TIME.sql"
+
+# 因为后面放到win2012上去，打成zip包
+zip -r $BACKUP_DIR/mysqlbak_$TIME.zip  $BACKUP_DIR/*.sql &>/dev/null
+[ $? -eq 0 ] && rm -rf $BACKUP_DIR/*.sql
+}
+
+bak_mysql
+# del zip before 10 days，执行脚本同时删除前10天的文件
+del_bef_10d() {
+find /home/mysqlbak -name "mysqlbak_*.zip" -a -mtime +10 -exec rm -rf {} \; &>/dev/null
+}
+
+del_bef_10d
+# upload backfiles to winserver2012
+curl -u $FTP_USER:$FTP_PASS ftp://$FTP_IP/ -X "MKD "$FTP_DIR &>/dev/null
+# 如果在win备份服务器上手动创建了文件夹，则无需在此执行创建
+
+# curl -u $FTP_USER:$FTP_PASS ftp://$FTP_IP/$FTP_DIR/ -T /home/mysqlbak/mysqlbak_*.zip
+up_file2win() {
+	cd /home/mysqlbak
+	ls mysqlbak_*.zip >jj
+
+	s=`cat jj | wc -l`
+	for((i=1;i<=$s;i++))
+	do {
+		up_file=`sed -n "$i"p jj`
+		echo upload file is $up_file
+		curl -u $FTP_USER:$FTP_PASS ftp://$FTP_IP/$FTP_DIR/ -T $up_file &>/dev/null
+	}
+	done
+}
+
+# 执行遍历，上传文件到win备份服务器
+up_file2win
